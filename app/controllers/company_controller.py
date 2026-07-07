b30a2683c3b2dd0a69be3f7a74fdd5c010889632
Smail Ferhi <___GitHub>
2026-07-08 01:43:05 +0200

    Complete company controller CRUD operations

--- app/controllers/company_controller.py
+++ app/controllers/company_controller.py
@@ -11,11 +11,27 @@ class CompanyController(BaseController):
     def __init__(self, service):
         super().__init__(service)
 
+
     def create_company(self, company):
+
         return self.service.create_company(company)
 
+
+    def get_company(self, company_id):
+
+        return self.service.get_company(company_id)
+
+
     def get_companies(self):
+
         return self.service.list_companies()
 
-    def deactivate(self, company_id):
+
+    def update_company(self, company):
+
+        return self.service.update_company(company)
+
+
+    def deactivate_company(self, company_id):
+
         return self.service.deactivate_company(company_id)
