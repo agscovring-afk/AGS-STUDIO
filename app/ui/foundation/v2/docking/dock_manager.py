7b258d21e43121b4644ceb59651a3b32ae4713c7
Smail Ferhi <___GitHub>
2026-07-11 21:38:35 +0200

    feat(ui): add AI_PANELS_AND_WINDOW_SYSTEM_V2

---
+++ app/ui/foundation/v2/docking/dock_manager.py
@@ -0,0 +1,9 @@
+class DockManager:
+
+    def __init__(self):
+
+        self.windows = []
+
+    def dock(self, window):
+
+        self.windows.append(window)
---
+++ app/ui/foundation/v2/statusbar/status_bar.py
@@ -0,0 +1,9 @@
+class StatusBar:
+
+    def __init__(self):
+
+        self.text = "Ready"
+
+    def set(self, text):
+
+        self.text = text
